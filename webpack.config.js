ce488e3be4f0c4dbba955b833d976a6890b5eeef
jaimelop83 <jaime.a.lopez@gmail.com>
2023-04-06 00:55:16 -0400

    added build directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
   module: {
     rules: [
       {
